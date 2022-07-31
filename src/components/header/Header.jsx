09b88722962dff8f9353d0bda7c6b31467e52874
Yogesh Ganesh Nanhe <yogeshnanhe2001@gmail.com>
2022-07-31 15:46:35 +0530

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,8 +14,10 @@ import "./header.css";
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({ type }) => {
+  const [destination, setDestination] = useState("");
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
@@ -31,6 +33,9 @@ const Header = ({ type }) => {
     room: 1,
   });
 
+
+  const navigate = useNavigate()
+
   const handleOption = (name, operation) => {
     setOptions(prev => {
       return {
@@ -38,6 +43,10 @@ const Header = ({ type }) => {
       }
     });
   };
+
+  const handleSearch = () =>{
+    navigate("/hotels", {state:{destination,date,options }});
+  }
 
   return (
     <div className="header">
@@ -79,6 +88,7 @@ const Header = ({ type }) => {
                   type="text"
                   placeholder="Where are you going?"
                   className="headerSearchInput"
+                  onChange={e=>setDestination(e.target.value)}
                 />
               </div>
               <div className="headerSearchItem">
@@ -90,6 +100,7 @@ const Header = ({ type }) => {
                   moveRangeOnFirstSelection={false}
                   ranges={date}
                   className="date"
+                  minDate={new Date()}
                 />}
               </div>
               <div className="headerSearchItem">
@@ -125,7 +136,7 @@ const Header = ({ type }) => {
                 </div>}
               </div>
               <div className="headerSearchItem">
-                <button className="headerBtn">Seach</button>
+                <button className="headerBtn" onClick={handleSearch}>Search</button>
               </div>
             </div></>}
       </div>
